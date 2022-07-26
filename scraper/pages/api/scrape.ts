774f27cba811077157dd976c2fda2e266241c441
Andreas Thomas <dev@chronark.com>
2022-07-26 14:11:33 +0200

    redeploy

--- scraper/pages/api/scrape.ts
+++ scraper/pages/api/scrape.ts
@@ -9,23 +9,31 @@ const redis = Redis.fromEnv();
  * Load the current bitcoin price in USD and store it in our database at the current timestamp
  */
 async function handler(_req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const bitcoinPrice = await fetch("https://blockchain.info/ticker").then(
-      (r) => r.json(),
-    ).then((r) => r["USD"]["last"]) as number;
+    try {
+        const bitcoinPrice = await fetch("https://blockchain.info/ticker").then(
+            (r) => r.json(),
+        ).then((r) => r["USD"]["last"]) as number;
 
-    await redis.zadd("bitcoin-prices", {
-      score: Date.now(),
-      member: bitcoinPrice,
-    });
+        await redis.zadd("bitcoin-prices", {
+            score: Date.now(),
+            member: bitcoinPrice,
+        });
 
-    res.send("OK");
-  } catch (err) {
-    res.status(500).send(err);
-  } finally {
-    res.end();
-  }
+        res.send("OK");
+    } catch (err) {
+        res.status(500).send(err);
+    } finally {
+        res.end();
+    }
 }
- export default verifySignature(handler, {
+export default verifySignature(handler, {
     currentSigningKey: process.env.QSTASH_CURRENT_SIGNING_KEY
- });
+});
+
+
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
